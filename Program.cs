df6b3d22016eed926ca3668a883ea8651aa487a1
victorazvdo29 <Josevictor16@yahoo.com.br>
2024-01-07 00:35:58 -0300

    commit projeto celular

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main()
+    {
+        var iphone = new Iphone("81999992233", "Iphone", "123456789101112", 128);
+        var nokia = new Nokia("81999994455", "Nokia", "123456789101112", 128);
+
+        iphone.InstalarAplicativo("Youtube");
+        nokia.InstalarAplicativo("Dio");
+    }
+}
